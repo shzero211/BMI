bc1fad1964052ab598c3b74039e262dbe2c071b4
SangHun <shzero211@naver.com>
2020-09-08 00:59:02 +0900

    second commit

--- app/src/main/java/com/example/bmi/BmiJavaActivity.java
+++ app/src/main/java/com/example/bmi/BmiJavaActivity.java
@@ -4,31 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class BmiJavaActivity extends AppCompatActivity {
-
+    EditText tallField;
+    EditText weightField;
+    TextView resultLabel;
+    Button bmiButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_view_binding);
-        findViewById(R.id.bmibutton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText tallField=findViewById(R.id.tall);
-                String tall=tallField.getText().toString();
 
-                EditText weightField=findViewById(R.id.weight);
-                String weight=weightField.getText().toString();
-                //bmi공식=체중/키*키(키는 미터로 계산하기때문에 100으로 나눠주고 실행)
-                double bmi=Double.parseDouble(weight)/Math.pow(Double.parseDouble(tall)/100.0,2);
+        //findviewByid 는 효율이 않좋기때문에 먼저 선언.
+    tallField=findViewById(R.id.tall);
+    weightField=findViewById(R.id.weight);
+    resultLabel=findViewById(R.id.result);
+    bmiButton=findViewById(R.id.bmibutton);
 
-                TextView resultLabel=findViewById(R.id.result);
-                resultLabel.setText("키:"+tall+",체중:"+weight+",BMI:"+bmi);
+    bmiButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String tall=tallField.getText().toString();
 
-            }
-        });
+            String weight=weightField.getText().toString();
+
+            double bmi=Double.parseDouble(weight)/Math.pow(Double.parseDouble(tall)/100.0,2);
+            resultLabel.setText("키:"+tall+",체중:"+weight+",BMI:"+bmi);
+        }
+    });
     }
 }
--- app/src/main/java/com/example/bmi/BmiKotlinActivity.kt
+++ app/src/main/java/com/example/bmi/BmiKotlinActivity.kt
@@ -2,10 +2,18 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.layout_view_binding.*
 
 class BmiKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_view_binding)
+        bmibutton.setOnClickListener{
+            val tallval=tall.text.toString().toDouble();
+            val weightval=weight.text.toString().toDouble();
+
+            val bmi=weightval/Math.pow(tallval/100.0,2.0)
+            result.text="키:${tall.text},체중:${weight.text},BMI:$bmi"
+        }
     }
 }
